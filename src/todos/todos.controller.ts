2993dc136d51e0327657c3817414ef8d21ebca67
Medo Rei <215371699+med0rei@users.noreply.github.com>
2025-08-27 13:00:48 +0900

    feat(todos): use dto in post and add put and delete methods

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
 
 @Controller('todos')
 export class TodosController {
@@ -8,7 +11,17 @@ export class TodosController {
   }
 
   @Post()
-  createTodo(): string {
+  createTodo(@Body() createTodoDto: CreateTodoDto): string {
     return 'This action creates a new todo';
+  }
+
+  @Put()
+  updateTodo(@Body() updateTodoDto: UpdateTodoDto): string {
+    return 'This action updates a todo';
+  }
+
+  @Delete()
+  deleteTodo(@Body('id') id: string): string {
+    return `This action deletes a todo with id ${id}`;
   }
 }

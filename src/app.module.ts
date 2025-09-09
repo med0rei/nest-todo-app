e5ae428b3310d15c3b283d0e6bb7429df9e8481b
Medo Rei <215371699+med0rei@users.noreply.github.com>
2025-09-09 13:18:01 +0900

    refactor(App): remove unused import and sort imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodosModule } from './todos/todos.module';
-import { Todo } from './todos/todo.entity';
-import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
